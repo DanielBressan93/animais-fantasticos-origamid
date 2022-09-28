52810b6ac0b14bcac0e904c7c76237127e84c3e4
DanielBressan93 <danielbressan18@gmail.com>
2022-09-28 09:16:49 -0300

    Primeiro Commit

---
+++ modules/fetch-bitcoin.js
@@ -0,0 +1,12 @@
+export default function initFetchBitcoin() {
+  fetch('https://blockchain.info/ticker')
+    .then((response) => {
+      response.json().then((btc) => {
+        const btcPreco = document.querySelector('.btc-preco');
+        btcPreco.innerText = (100 / btc.BRL.sell).toFixed(4);
+      });
+    })
+    .catch((erro) => {
+      console.log(Error(erro));
+    });
+}
